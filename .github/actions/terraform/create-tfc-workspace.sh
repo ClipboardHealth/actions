f402b805c9d49c70bc82ced02c27f62fa264158f
Zlash65 <zarrar65@gmail.com>
2022-05-26 17:39:36 -0400

    fix: workspace creation and deletion composite action

--- .github/actions/terraform/create-tfc-workspace.sh
+++ .github/actions/terraform/create-tfc-workspace.sh
@@ -1,6 +1,4 @@
-#!/bin/bash
-
-set -e
+#!/bin/bash -e
 
 TFC_URL_PREFIX="https://app.terraform.io/api/v2"
 TFC_WORKSPACES_URL="${TFC_URL_PREFIX}/organizations/clipboard-staffing/workspaces"
@@ -8,16 +6,16 @@ TFC_WORKSPACE_NAME="${SERVICE_NAME}-${ENVIRONMENT_NAME}"
 HDR_AUTH="Authorization: Bearer ${TERRAFORM_TOKEN}"
 HDR_CNTT="Content-Type: application/vnd.api+json"
 
-echo "Get existant workspace id"
+echo "Get existent workspace id"
 TFC_GET_WORKSPACE_URL="${TFC_WORKSPACES_URL}/${TFC_WORKSPACE_NAME}"
 curl -H "$HDR_AUTH" -H "$HDR_CNTT" -s "$TFC_GET_WORKSPACE_URL" -o workspace.json
 WORKSPACE_ID=$(cat workspace.json | jq -r .data.id)
 
 # Create a new workspace if doesn't exists
-if [ "$WORKSPACE_ID" = "null" ]; then
-  echo "Creating new workspace, since it doesn't exists"
+if [[ "$WORKSPACE_ID" == "null" ]]; then
+  echo "Creating new workspace, since it doesn't exist"
 
-  cat <<EOF >new_workspace.json
+cat <<EOF >new_workspace.json
   {"data": {
     "attributes": {"name": "${TFC_WORKSPACE_NAME}"},
     "type": "workspaces"
@@ -36,25 +34,26 @@ WORKSPACE_NAME=$(cat workspace.json | jq -r .data.attributes.name)
 WORKSPACE_TAGS=$(cat workspace.json | jq -r '.data.attributes["tag-names"][]' | sort)
 
 echo
-echo "Workspace name: $WORKSPACE_NAME"
 echo "Workspace ID:   $WORKSPACE_ID"
+echo "Workspace name: $WORKSPACE_NAME"
+echo "Workspace tags: $WORKSPACE_TAGS"
 echo
 
 COMMA_TAG_LIST="${TERRAFORM_TAG_LIST},${ENVIRONMENT_NAME},${SERVICE_NAME}"
-echo "$COMMA_TAG_LIST" | sed -n 1'p' | tr ',' '\n' | sort >tag_list.txt
+echo "$COMMA_TAG_LIST" | sed -n 1'p' | tr ',' '\n' | sort > tag_list.txt
 
 if [ "$(cat tag_list.txt)" != "$WORKSPACE_TAGS" ]; then
   echo "Tags didn't match, update workspace tags"
   while read tag; do
     JSON_TAGS=$JSON_TAGS',{"type": "tags", "attributes": {"name": "'$tag'"}}'
-  done <tag_list.txt
+  done < tag_list.txt
 
   TFC_ADD_TAG_URL="${TFC_URL_PREFIX}/workspaces/${WORKSPACE_ID}/relationships/tags"
-  echo "{\"data\": [${JSON_TAGS:1}]}" >workspace_tags.json
+  echo "{\"data\": [${JSON_TAGS:1}]}" > workspace_tags.json
   curl -H "$HDR_AUTH" -H "$HDR_CNTT" -s \
     --request POST -d @workspace_tags.json \
     $TFC_ADD_TAG_URL
 fi
 
 echo "Remove temporary files"
-rm $(dirname $0)/*.{json,txt}
+rm $(dirname $0)/*.{json,txt} || echo
--- .github/actions/terraform/destroy-tfc-workspace.sh
+++ .github/actions/terraform/destroy-tfc-workspace.sh
@@ -1,15 +1,14 @@
-#!/bin/bash
+#!/bin/bash -e
 
-set -e
-
+TFC_URL_PREFIX="https://app.terraform.io/api/v2"
+TFC_WORKSPACES_URL="${TFC_URL_PREFIX}/organizations/clipboard-staffing/workspaces"
 TFC_WORKSPACE_NAME="${SERVICE_NAME}-${ENVIRONMENT_NAME}"
 HDR_AUTH="Authorization: Bearer ${TERRAFORM_TOKEN}"
 HDR_CNTT="Content-Type: application/vnd.api+json"
 
-echo "Get existant workspace id"
-curl -H "$HDR_AUTH" -H "$HDR_CNTT" -s \
-  "https://app.terraform.io/api/v2/organizations/clipboard-staffing/workspaces/${TFC_WORKSPACE_NAME}" \
-  -o workspace.json
+echo "Get existent workspace id"
+TFC_GET_WORKSPACE_URL="${TFC_WORKSPACES_URL}/${TFC_WORKSPACE_NAME}"
+curl -H "$HDR_AUTH" -H "$HDR_CNTT" -s "$TFC_GET_WORKSPACE_URL" -o workspace.json
 WORKSPACE_ID=$(cat workspace.json | jq -r .data.id)
 
 echo
@@ -18,17 +17,17 @@ echo "Workspace ID:   $WORKSPACE_ID"
 echo
 
 # Create a new workspace if doesn't exists
-if [ "$WORKSPACE_ID" = "null" ]; then
-  echo "Workspace does exists, so cannot be deleted"
+if [ "$WORKSPACE_ID" == null ]; then
+  echo "Workspace does not exist, so cannot be deleted"
   exit 1
 fi
 
-curl -H "$HDR_AUTH" -H "$HDR_CNTT" -s \
-  --request DELETE \
-  -o destroy_workspace.json \
-  "https://app.terraform.io/api/v2/admin/workspaces/${WORKSPACE_ID}"
+echo "Delete workspace if it exists"
+TFC_DELETE_WORKSPACE_URL="${TFC_WORKSPACES_URL}/${TFC_WORKSPACE_NAME}"
+curl -H "$HDR_AUTH" -H "$HDR_CNTT" -s --request DELETE -o destroy_workspace.json "$TFC_DELETE_WORKSPACE_URL"
+WORKSPACE_ID=$(curl -H "$HDR_AUTH" -H "$HDR_CNTT" -s "$TFC_GET_WORKSPACE_URL" | jq -r .data.id)
 
-if [ "$(cat destroy_workspace.json | jq -r '.errors')" = "null" ]; then
+if [ "$WORKSPACE_ID" == "null" ]; then
   echo "Workspace destroyed!"
 else
   echo "Error during workspace destruction: " \
@@ -37,4 +36,4 @@ else
 fi
 
 echo "Remove temporary files"
-rm destroy_workspace.json
+rm destroy_workspace.json || echo
